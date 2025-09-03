b8ec1cc1287b4a3cd5b00827760e8f3bfcbdcac5
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-09-03 15:33:02 -0300

    add files

---
+++ 01.introduction_to_python/01.variables_and_types.py
@@ -0,0 +1,25 @@
+# BMI
+height = 1.79
+weight = 68.7
+
+bmi = weight / height ** 2
+print(bmi)
+print(type(bmi)) # Float
+
+day_of_week = 5
+print(day_of_week)
+print(type(day_of_week)) #Int
+
+
+x = "body mass index"
+y = "this works too"
+print(type(x))
+
+z = True
+print(type(z))
+
+sum = 2 + 3
+print(sum)
+
+sum = "ab" + "cd"
+print(sum)
