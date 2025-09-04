6995d82f47bed0e3d611f80e67756bf4bab56102
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-09-04 16:52:58 -0300

    update

---
+++ datacamp/01.introduction_to_python/02.python_lists.py
@@ -0,0 +1,11 @@
+fam = [1.73,1.68,1.71,1.89]
+print(fam)
+print(type(fam))
+
+fam2 = ["liz",1.73,"emma",1.68,"mom",1.71,"dad",1.89]
+print(fam2)
+print(type(fam2))
+
+fam3 = [["liz",1.73],["emma",1.68],["mom",1.71],["dad",1.89]]
+print(fam3)
+print(type(fam3))
---
+++ datacamp/01.introduction_to_python/exercise/03.variable_assignment.py
@@ -0,0 +1,10 @@
+# Instructions
+# Create a variable savings with the value of 100.
+# Check out this variable by typing print(savings) in the script.
+
+# Execise
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
