fa28504625606a1f2c8a55014627754bbd31022f
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-09-08 18:01:00 -0300

    update

---
+++ datacamp/01.introduction_to_python/exercises/12.subsetting_lists_of_lists.py
@@ -0,0 +1,14 @@
+# Instructions
+# Subset the house list to get the float 9.5.
+
+
+# Exercise
+
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
+
+# Subset the house list
+house[4][1]
