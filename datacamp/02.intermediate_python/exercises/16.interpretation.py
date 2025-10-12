82e44d4db229daa368d9dfa917c1457ec4ce3955
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-10-12 17:32:08 -0300

    update

---
+++ datacamp/02.intermediate_python/exercises/16.interpretation.py
@@ -0,0 +1,7 @@
+# Instructions
+# Possible answers
+
+# The countries in blue, corresponding to Africa, have both low life expectancy and a low GDP per capita. - Correct
+# There is a negative correlation between GDP per capita and life expectancy. - Incorrect
+# China has both a lower GDP per capita and lower life expectancy compared to India. - Incorrect
+
